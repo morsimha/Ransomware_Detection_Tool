14fee8ffd4160e6cc311fc9fdf0177f366fabada
morsimha <mor.simha@intel.com>
2022-12-16 10:02:00 +0200

    real first commit

---
+++ SusFile.py
@@ -0,0 +1,93 @@
+import math
+import os.path
+import magic
+
+class SusFile:
+    def __init__(self, path):
+        self.path = str(path)
+        self.file_name = os.path.basename(self.path)
+        self.meta_data = os.stat(self.path)
+        self.size = os.path.getsize(self.path)
+        self.meta_data_size = ''
+        self.date_modified = ""
+
+        # self.snap_diff_size = [1,2,3,4,5,6,7,8,9,10]
+        # self.date_modified = ""
+        # self.date_modified = ""
+        # self.is_encrypted = True
+        # self.signature = ''
+
+        self.grade = 100  # start by suspect. 100 is severe.
+       # self.next_snap_grade = self.grade  # start by suspect. 100 is severe.
+
+    # def compare_file(self):
+    #     pass
+
+
+    def get_size(self):
+        return self.size
+
+    def collect_metadata_content(self):
+        self.meta_data_size = self.meta_data.st_size
+        self.date_modified = self.meta_data.st_mtime
+
+
+    def compare(self, old_file, new_file):
+        self.compare_size(old_file, new_file)
+        self.compare_metadata(old_file, new_file)
+        self.compare_time_modified(old_file, new_file)
+
+    def compare_size(self, old_file,new_file):
+        # Compare the metadata for each file
+        if old_file.get_size() != new_file.size:
+            pass
+        else:
+            self.grade -= 10
+        return
+
+    def compare_metadata(self, old_file, new_file):
+        # Compare the metadata for each file
+        if old_file.meta_data_size != new_file.meta_data_size:
+            pass
+        else:
+            self.grade -= 10
+
+        return new_file.size - old_file.get_size()
+
+    def compare_time_modified(self, old_file, new_file):
+        # Compare the metadata for each file
+        if old_file.date_modified != new_file.date_modified:
+            pass
+        else:
+            self.grade -= 10
+
+
+    def compare_file_format(self):
+        # Compare the metadata for each file
+        file_format = magic.from_file(self.path)
+     #   print(file_format)
+        if file_format != "data":
+            self.grade -= 20
+        else:
+            pass
+
+    def compare_antropy(self):
+        with open(self.path, 'rb') as file:
+            data = file.read()
+        ent = 0.0
+        if len(data) < 2:
+            return ent
+        size = float(len(data))
+        for b in range(256):
+            freq = data.count(b)
+            if freq > 0:
+                freq = float(freq) / size
+                ent = ent + freq * math.log(freq, 2)
+       # print(-ent / 8 > 0.8)
+        if (-ent / 8) > 0.8: #bad because infected true
+            pass
+        else:
+            self.grade -= 50
+        # else:
+        #     print("not anttttttttttttttttttttttttttttt")
+        return (-ent / 8) > 0.8
---
+++ main.py
@@ -0,0 +1,89 @@
+import os
+from pathlib import Path
+
+from SusFile import SusFile
+
+abs_path = Path(r'C:\Users\morsimha\OneDrive - Intel Corporation\Desktop\Hacathon toga\Flash')
+
+vols = list(os.listdir(abs_path))
+snaps = []
+
+for x in (range(10)):
+    snaps.append(f'snapshot_{x+1}')
+
+print(vols)
+print(snaps)
+
+data = {}
+
+diff_list = []
+vol1 = []
+vol2 = []
+vol3 = []
+
+infected_vols = [vol1,vol2,vol3]
+
+vol1_log = []
+vol2_log = []
+vol3_log = []
+
+infected_vols_logs = [vol1_log,vol2_log,vol3_log]
+i = 0
+curr_infected_list_log = infected_vols_logs[i]
+
+curr_infected_list = infected_vols[i]
+old_snap = ""
+
+for vol in vols:
+    data[f'{vol}'] = {}
+    for snap in snaps:
+        data[f'{vol}'][f'{snap}'] = {}
+        for filename in os.listdir(abs_path / str(vol) / str(snap)):
+            # Colecting data
+            current_path = abs_path / str(vol) / str(snap) / str(filename)
+            current_size = os.path.getsize(current_path)
+            # Add susFile object to dictionary
+            data[f'{vol}'][f'{snap}'][f'{filename}'] = SusFile(current_path)  # init new file obj
+            currFile = data[f'{vol}'][f'{snap}'][f'{filename}']
+            currFile.collect_metadata_content()
+            # check if can compare
+            if not snap == 'snapshot_1':
+                old_path = abs_path / str(vol) / str(old_snap) / str(filename)
+                if os.path.exists(old_path):
+                    old_file = data[f'{vol}'][f'{old_snap}'][f'{filename}']
+                    currFile.compare(old_file, currFile)
+                    #  checks per file
+                    currFile.compare_file_format()
+                    if currFile.grade != 70: # no changes at all
+                        currFile.compare_antropy()
+
+
+                    if currFile.grade > 70:
+                        msg = currFile.file_name
+                        if msg not in curr_infected_list:
+                            print(msg)
+                            curr_infected_list.append(msg)
+                            msg = f"{currFile.file_name} \t infect at \t{snap}"
+                            curr_infected_list_log.append(msg)
+
+
+        ###### end of snapshot run
+
+        old_snap = str(snap)
+
+    with open(f"{vol}_file_list.txt", "w") as f:
+        for line in curr_infected_list:
+            f.writelines(f'{line}\n')
+        f.writelines(f'number of files in current vol: {len(curr_infected_list_log)}\n')
+
+
+    with open(f"{vol}_log_file.txt", "w") as f:
+        for line in curr_infected_list_log:
+            f.writelines(f'{line}\n')
+        f.writelines(f'number of files in current vol: {len(curr_infected_list_log)}\n')
+
+    i += 1
+    if i<3:
+        curr_infected_list = infected_vols[i]
+        curr_infected_list_log = infected_vols_logs[i]
+
